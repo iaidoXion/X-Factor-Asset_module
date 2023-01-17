ee0211b2f208bf402e88dbf3df88ac3113f0b06b
iaido <iaido@xionits.com>
2023-01-17 17:17:22 +0900

    스케줄링 구현

--- main2.py
+++ main2.py
@@ -9,22 +9,27 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def minutely() :
     if CMU == 'true':
+        now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        print('minutely')
+        print(now)
         CTMPI()
     else:
         logging.info('Tanium Minutely cycle 사용여부  : ' + CMU)
 
 def daily():
     if CDU == 'true' :
+        now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        print('daily')
+        print(now)
         CTDPI()
     else:
         logging.info('Tanium Daily cycle 사용여부  : ' + CDU)
 
 def main():
     if TU == 'true':
-        print(type(CMT))
         sched = BlockingScheduler(timezone='Asia/Seoul')
-        sched.add_job(minutely, 'interval', seconds=5, args=['interval'])  # minute='3'
-        sched.add_job(daily, 'cron', hour='00', minute='00', args=['cron'])
+        sched.add_job(minutely, 'interval', seconds=CMT)  # seconds='3'
+        sched.add_job(daily, 'cron', hour=CDTH, minute=CDTM)
         sched.start()
     else:
         logging.info('Tanium 사용여부 : '+TU)
@@ -39,6 +44,8 @@ if __name__ == "__main__":
     CMU = SETTING['CORE']['Tanium']['CYCLE']['MINUTELY']['USE']
     CMT = SETTING['CORE']['Tanium']['CYCLE']['MINUTELY']['TIME']
     CDU = SETTING['CORE']['Tanium']['CYCLE']['DAILY']['USE']
+    CDTH = SETTING['CORE']['Tanium']['CYCLE']['DAILY']['TIME']['hour']
+    CDTM = SETTING['CORE']['Tanium']['CYCLE']['DAILY']['TIME']['minute']
 
     today = datetime.today().strftime("%Y%m%d")
     logFile = LOGFD + LOGFNM + today + LOGFF
